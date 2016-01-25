1afac4ab954cf880171312fa2fbada307843ac48
Joshua Masland-Sarani <j.masland@gmail.com>
2016-01-25 11:46:24 -0500

    first

--- app.rb
+++ app.rb
@@ -3,3 +3,18 @@ require_relative 'lib/tower_of_hanoi'
 
 enable :sessions
 
+get '/' do
+  @toh = TowerOfHanoi.new(session['towers'])
+
+  
+
+  session['towers'] = @toh.towers
+
+  erb :game, locals: { towers: @toh.towers }
+end
+
+post '/move' do
+  @toh = TowerOfHanoi.new
+
+  redirect '/'
+end
